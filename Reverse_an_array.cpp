c02ced0cac0d711112be52a5a960df6dfa7372d4
Astha Gupta <148465339+ASTHA864@users.noreply.github.com>
2025-02-08 19:22:40 +0530

    Add files via upload

---
+++ Reverse_an_array.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+    public:
+      void reverseArray(vector<int> &arr) {
+          // code here
+          int n=arr.size();
+          int start=0;
+          int end=n-1;
+          while(start<=end)
+          {
+              swap(arr[start],arr[end]);
+              start++;
+              end--;
+          }
+      }
+  };
